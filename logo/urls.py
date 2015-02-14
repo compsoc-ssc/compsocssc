5ff4f9148251bc59f8ec4683ef4e656036127a06
Ratik <Ratik96@gmail.com>
2015-02-15 00:17:08 +0530

    Restructured logo app views

--- logo/urls.py
+++ logo/urls.py
@@ -6,5 +6,4 @@ urlpatterns=patterns('',
     url(r'^$', views.home, name='home'),
     url(r'^submission/$', views.submission, name='submission'),
     url(r'^success/$', views.success, name='success'),
-    # url(r'^instructions/$', views.instructions, name='instructions'),
 )
