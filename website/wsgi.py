02a03348a51fe7855c1e3592859c34be45359383
Ratik Sharma <Ratik96@gmail.com>
2015-07-22 16:23:26 +0530

    Add production settings

--- website/wsgi.py
+++ website/wsgi.py
@@ -13,3 +13,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
+
+try:
+    from dj_static import Cling
+
+    application = Cling(get_wsgi_application())
+except:
+    pass
