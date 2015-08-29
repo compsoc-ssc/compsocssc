8ce1f9d131ed3e442f4dc8aa6c349e983c9949f1
Ratik Sharma <Ratik96@gmail.com>
2015-08-29 21:16:08 +0530

    Remove Heroku config

--- website/wsgi.py
+++ website/wsgi.py
@@ -13,10 +13,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
-
-try:
-    from dj_static import Cling
-
-    application = Cling(get_wsgi_application())
-except:
-    pass
