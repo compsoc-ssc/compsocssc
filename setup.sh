9c69c3b61c20f6733093379f90a69e992f20a736
Ratik Sharma <Ratik96@gmail.com>
2016-08-13 01:51:47 +0530

    Add setup file

---
+++ setup.sh
@@ -0,0 +1,20 @@
+printf "######### STARTING SETUP #########\n"
+printf "#################################\n"
+printf "\nCreating and activating a virtual environment\n"
+printf "#################################\n"
+virtualenv -p python3 django
+source django/bin/activate
+printf "\nInstalling dependencies\n"
+printf "#################################\n"
+pip install -r requirements.txt
+printf "\nSetting up the database\n"
+printf "#################################\n"
+touch website/settings/local.py
+python manage.py makemigrations && python manage.py migrate
+printf "\nCreate a superuser -\n"
+python manage.py createsuperuser
+printf "#################################"
+printf "\nSetup complete :)"
+printf "\nStarting a development server...\n"
+python manage.py runserver
+
